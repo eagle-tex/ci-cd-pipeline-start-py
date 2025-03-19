37d2eebf0a391aac576f5cf02a970fe6f90c5b1a
eagle-tex <70228400+eagle-tex@users.noreply.github.com>
2025-03-19 16:10:15 +0100

    feat: add subtract function and test case

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,13 @@
-from src.main import add
+from src.main import add, subtract
 
 
 def test_add_function():
     assert add(2, 3) == 5
-    assert add(0, 0) == 1
+    assert add(0, 0) == 0
     assert add(5, 7) == 12
+
+
+def test_subtract_function():
+    assert subtract(5, 3) == 2
+    assert subtract(1, 2) == -1
+    assert subtract(17, 8) == 9
